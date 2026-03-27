c9bb332aadcc66d204ef4ebd1acaa4e0784ea4e8
Ardita Natalia <arditanatalia.2023@student.uny.ac.id>
2026-03-27 13:07:19 +0700

    Masuk Github

---
+++ Pertemuan 6/Latihan_2.js
@@ -0,0 +1,104 @@
+const btnStart = document.getElementById('btnStart');
+const btnStop = document.getElementById('btnStop');
+const btnReset = document.getElementById('btnReset');
+const btnMaintenance = document.getElementById('btnMaintenance');
+
+const statusIndicator = document.getElementById('statusIndicator');
+const suhuMesin = document.getElementById('suhuMesin');
+const teksStatus = statusIndicator.querySelector('strong');
+const panel = document.querySelector('.panel');
+
+let suhu = 25;
+let intervalId = null;
+
+btnStart.addEventListener('click', function () {
+
+    statusIndicator.style.background = "#27ae60"; // hijau
+    statusIndicator.style.color = "white";
+    teksStatus.innerText = "RUNNING";
+
+    intervalId = setInterval(function () {
+        suhu++;
+        suhuMesin.innerText = suhu + " °C";
+
+        if (suhu > 80) {
+            statusIndicator.style.background = "#e74c3c"; // merah
+            teksStatus.innerText = "OVERHEAT WARNING";
+            suhuMesin.style.color = "red";
+        }
+    }, 1000);
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+});
+
+
+btnStop.addEventListener('click', function () {
+    clearInterval(intervalId);
+
+    statusIndicator.style.background = "#e74c3c"; // 🔴 MERAH
+    statusIndicator.style.color = "white";
+    teksStatus.innerText = "STOPPED";
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+});
+
+
+btnReset.addEventListener('click', function () {
+    clearInterval(intervalId);
+
+    suhu = 25;
+    suhuMesin.innerText = suhu + " °C";
+    suhuMesin.style.color = "black";
+
+    statusIndicator.style.background = "#f1c40f"; // 🟡 KUNING
+    statusIndicator.style.color = "black";
+    teksStatus.innerText = "RESET";
+
+    panel.classList.remove('bg-light');
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+});
+
+
+btnMaintenance.addEventListener('click', function () {
+
+    panel.classList.add('bg-light');
+
+    statusIndicator.style.background = "#bdc3c7"; // ⚪ ABU
+    statusIndicator.style.color = "black";
+    teksStatus.innerText = "MAINTENANCE";
+});
+
+
+// =======================
+// VALIDASI RPM
+// =======================
+const inputRPM = document.getElementById('inputRPM');
+const pesanError = document.getElementById('pesanError');
+
+inputRPM.addEventListener('input', function () {
+    let val = parseInt(this.value);
+
+    if (val > 2000) {
+        pesanError.style.display = "block";
+    } else {
+        pesanError.style.display = "none";
+    }
+});
+
+// =======================
+// LATIHAN 2 (MOUSE EVENT)
+// =======================
+
+suhuMesin.addEventListener('mouseover', function () {
+    suhuMesin.style.color = "blue";
+    suhuMesin.style.fontWeight = "bold";
+});
+
+suhuMesin.addEventListener('mouseout', function () {
+    suhuMesin.style.color = "black";
+    suhuMesin.style.fontWeight = "normal";
+});
