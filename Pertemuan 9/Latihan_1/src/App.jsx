f7bc9b1317ac1cccaaede84b8877ea0d2742f0da
Ardita Natalia <arditanatalia.2023@student.uny.ac.id>
2026-04-24 17:16:39 +0700

    Masuk ke Github

---
+++ Pertemuan 9/Latihan_1/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+// Import komponen yang sudah dibuat
+import KartuMesin from './Komponen/KartuMesin';
+
+function App() {
+  return (
+    <div className="container mt-4">
+
+      <div className="text-center mb-4">
+        <h1>Monitoring Lini Produksi A</h1>
+        <p className="text-muted">
+          Latihan 1 - Destructuring Props
+        </p>
+      </div>
+
+      <div className="row">
+        {/* Kolom 1 */}
+        <div className="col-md-4">
+          <KartuMesin
+            nama="CNC-Turning-01"
+            status="Running"
+            produksi={150}
+          />
+        </div>
+
+        {/* Kolom 2 */}
+        <div className="col-md-4">
+          <KartuMesin
+            nama="CNC-Milling-02"
+            status="Maintenance"
+            produksi={0}
+          />
+        </div>
+
+        {/* Kolom 3 */}
+        <div className="col-md-4">
+          <KartuMesin
+            nama="Press-Hydraulic-05"
+            status="Stop"
+            produksi={85}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
